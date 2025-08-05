0a4622529e326ecc5be57c3a484f6659d6873aba
Bhavesh <146314561+BhaveshBytess@users.noreply.github.com>
2025-08-05 22:00:14 +0530

    Update CODE.cpp

--- GRAPHS/NO. OF PROVINCES/CODE.cpp
+++ GRAPHS/NO. OF PROVINCES/CODE.cpp
@@ -33,4 +33,38 @@ public:
 
 
 //BFS
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> visited(n, false);
+        int provinces = 0;
 
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                provinces++;
+                bfs(i, isConnected, visited);
+            }
+        }
+
+        return provinces;
+    }
+
+    void bfs(int start, vector<vector<int>>& isConnected, vector<bool>& visited) {
+        queue<int> q;
+        q.push(start);
+        visited[start] = true;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int neighbor = 0; neighbor < isConnected.size(); ++neighbor) {
+                if (isConnected[node][neighbor] == 1 && !visited[neighbor]) {
+                    q.push(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+    }
+};
