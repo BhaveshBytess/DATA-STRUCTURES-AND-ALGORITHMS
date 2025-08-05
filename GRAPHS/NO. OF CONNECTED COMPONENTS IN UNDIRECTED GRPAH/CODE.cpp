a01c25f4e1bfc7e9588bae10a5e185a9d5e39ab0
Bhavesh <146314561+BhaveshBytess@users.noreply.github.com>
2025-08-05 21:56:00 +0530

    Create CODE.cpp

---
+++ GRAPHS/NO. OF CONNECTED COMPONENTS IN UNDIRECTED GRPAH/CODE.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+        visited[node] = true;
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited);
+            }
+        }
+    }
+
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n); // adjacency list
+
+        // Build graph
+        for (auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u); // because undirected
+        }
+
+        vector<bool> visited(n, false);
+        int components = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                components++;        // new component found
+                dfs(i, adj, visited);
+            }
+        }
+
+        return components;
+    }
+};
