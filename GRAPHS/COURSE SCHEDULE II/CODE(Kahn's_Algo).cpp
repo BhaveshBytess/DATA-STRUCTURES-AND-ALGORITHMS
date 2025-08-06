3d9447177dfe8b9a483416eaabe01c17a2bdd54d
Bhavesh <146314561+BhaveshBytess@users.noreply.github.com>
2025-08-07 00:27:07 +0530

    Update CODE(Kahn's_Algo).cpp

--- GRAPHS/COURSE SCHEDULE II/CODE(Kahn's_Algo).cpp
+++ GRAPHS/COURSE SCHEDULE II/CODE(Kahn's_Algo).cpp
@@ -1,1 +1,41 @@
-
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        vector<int> indegree(numCourses, 0);
+        
+        // Build adjacency list and compute indegrees
+        for (auto& pre : prerequisites) {
+            int u = pre[1], v = pre[0];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        
+        // Push all nodes with indegree 0 into queue
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+        
+        vector<int> topoOrder;
+        
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topoOrder.push_back(node);
+            
+            for (int neighbor : adj[node]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0)
+                    q.push(neighbor);
+            }
+        }
+        
+        // If all courses are not included, return empty
+        if (topoOrder.size() != numCourses)
+            return {};
+        
+        return topoOrder;
+    }
+};
